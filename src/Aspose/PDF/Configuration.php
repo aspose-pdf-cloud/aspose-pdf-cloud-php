aeccb3b18c9bcd3ea017399155081cfde5433d75
Andrey Kaferi <andrey.kaferi@aspose.com>
2018-08-02 16:30:27 +0300

    Updated to 18.7

--- src/Aspose/PDF/Configuration.php
+++ src/Aspose/PDF/Configuration.php
@@ -397,7 +397,7 @@ class Configuration
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
         $report .= '    OpenAPI Spec Version: 1.1' . PHP_EOL;
-        $report .= '    SDK Package Version: 18.6.0' . PHP_EOL;
+        $report .= '    SDK Package Version: 18.7.0' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;
